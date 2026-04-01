d542e2b738bd0c6fbe6dfda70d5392ecbd833771
SluchCr7 <ahmedabobakr2972004@gmail.com>
2026-04-01 21:40:37 +0200

    Big update back and front

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000',
+        url: process.env.SERVER_LINK || 'http://localhost:5000',
         description: 'Development server',
       },
     ],
